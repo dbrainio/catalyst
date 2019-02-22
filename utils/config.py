d6c0908f17f2a53e360b50415b4849354c40e3fc
Sergey Kolesnikov <scitator@gmail.com>
2019-02-22 18:51:44 +0300

    ascii fix

--- utils/config.py
+++ utils/config.py
@@ -25,7 +25,7 @@ def load_ordered_yaml(
 def save_config(config, logdir):
     os.makedirs(logdir, exist_ok=True)
     with open("{}/config.json".format(logdir), "w") as fout:
-        json.dump(config, fout, indent=2)
+        json.dump(config, fout, indent=2, ensure_ascii=False)
 
 
 def parse_config_args(*, config, args, unknown_args):
